b1b6df25419997691a7640c1173ae8025e27a401
Shin <whitefusion@163.com>
2018-02-22 16:35:50 +0000

    update database file

--- src/service/model.py
+++ src/service/model.py
@@ -7,7 +7,6 @@ from sqlalchemy import create_engine
 from passlib.apps import custom_app_context as pwd_context
 
 Base = declarative_base()
-secret_key = ''.join(random.choice(string.ascii_uppercase+string.digits) for x in xrange(32))
 
 class User(Base):
     __tablename__='user'
