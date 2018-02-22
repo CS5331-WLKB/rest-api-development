e9e4cee9f1cf89fed113d3b2a4db4207795a655e
Shin <whitefusion@163.com>
2018-02-22 16:31:06 +0000

    add token table

--- src/service/model.py
+++ src/service/model.py
@@ -7,33 +7,51 @@ from sqlalchemy import create_engine
 from passlib.apps import custom_app_context as pwd_context
 
 Base = declarative_base()
+secret_key = ''.join(random.choice(string.ascii_uppercase+string.digits) for x in xrange(32))
 
 class User(Base):
     __tablename__='user'
 
     id=Column(Integer,primary_key=True)
     username=Column(String(32),index=True,nullable=False)
-    password_hash=Column(String(64))
-    fullname=Column(String(32),nullable=False)
-    age=Column(Integer)
+    password_hash=Column(String(64),nullable=False)
+    fullname=Column(String(100),nullable=False)
+    age=Column(Integer, nullable=False)
 
     def hash_password(self,password):
         self.password_hash=pwd_context.encrypt(password)
 
     def verify_password(self,password):
         return pwd_context.verify(password,self.password_hash)
+
     
 class Diary(Base):
     __tablename__='diary'
 
     id=Column(Integer,primary_key=True)
     title=Column(String(100),nullable=False)
+    author=Column(String(32),ForeignKey('user.username'),index=True)
     publish_date=Column(Date,nullable=False)
     public = Column(Boolean,nullable=False)
     text=Column(String,nullable=False)
-
-    user_id=Column(Integer,ForeignKey('user.id'))
     user=relationship(User)
+    
+    @property
+    def serialize(self):
+        return {
+            'id':self.id,
+            'title':self.title,
+            'author': self.author,
+            'publish_date':self.publish_date,
+            'public': self.public,
+            'text': self.text
+        }
+        
+class Token(Base):
+    __tablename__='token'
+    id = Column(Integer,primary_key=True)
+    uuid=Column(String, index=True,nullable=False)
+    expired=Column(Boolean,nullable=False)
 
 engine = create_engine('sqlite:///secretdiary.db')
 Base.metadata.create_all(engine)
