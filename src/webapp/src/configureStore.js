a1aab5674c9fb0bc081bf1f925bac061932bfd32
Liu Chao <luis.liu.1018@gmail.com>
2018-03-03 10:58:54 +0800

    Add devTool dev dependency

--- src/webapp/src/configureStore.js
+++ src/webapp/src/configureStore.js
@@ -2,12 +2,15 @@ import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import rootReducer from './reducers';
+import {composeWithDevTools} from 'redux-devtools-extension';
 const loggerMiddleware = createLogger();
 
 export default function configureStore(preloadedState) {
   return createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunkMiddleware, loggerMiddleware)
+    composeWithDevTools(
+      applyMiddleware(thunkMiddleware, loggerMiddleware)
+    )
   );
 }
